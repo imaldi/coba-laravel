6a55e2367497ba75fc848a65d00d0736aca310db
imaldi <aldiirsanmajid@gmail.com>
2022-02-10 16:59:54 +0700

    done separating data from routes with models

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,5 +8,5 @@
         <h5>By: {{ $post['author'] }}</h5>
         <p>{{ $post['body'] }}</p>
     </article>
-    <a href="/blog">Back to Posts</a>
+    <a href="/posts">Back to Posts</a>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,51 +31,16 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            'title' => 'Judul Post Pertama',
-            'slug' => 'judul-post-pertama',
-            'author' => 'Aldi Majide',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit non ipsum incidunt, ullam atque magnam itaque hic assumenda optio nostrum eligendi doloribus quo fuga dolore reprehenderit officia? Libero hic cumque ad vero autem quae? Culpa deleniti amet explicabo porro cupiditate molestiae sint laudantium. Quam molestiae ratione sed facilis. Eius minima possimus maiores pariatur delectus, dolor in modi quod dolorum repellendus? Magni, illo alias debitis officia perspiciatis doloribus fuga dolorum. Commodi quo sequi odit distinctio ex optio possimus repellat, laborum rem?'
-        ],
-        [
-            'title' => 'Judul Post Kedua',
-            'slug' => 'judul-post-kedua',
-            'author' => 'Uhuy',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias architecto nobis voluptate nesciunt eos repellat doloribus quas animi ducimus rem. Quidem iure nihil ducimus ea at eveniet voluptate libero laborum quibusdam repudiandae. Adipisci fugit rem provident, eligendi iure ducimus eveniet asperiores assumenda. Possimus ex asperiores quod animi odit enim optio corrupti, soluta voluptates quam tempore ipsa iusto provident reiciendis ullam repellat quo magnam adipisci, cupiditate dolorem incidunt voluptatibus maiores nemo ipsum! Veniam adipisci accusantium porro et. Quis quaerat omnis eos error maxime ratione tempore, asperiores labore sapiente temporibus! Alias, sequi odit ea totam iusto cupiditate cumque facere iure tempora. Magni?'
-        ],
-    ];
+Route::get('/posts', function () {
     return view('posts', [
         'title' => 'Posts',
-        'posts' => $blog_posts
+        'posts' => Post::all()
     ]);
 });
 
 Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            'title' => 'Judul Post Pertama',
-            'slug' => 'judul-post-pertama',
-            'author' => 'Aldi Majide',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit non ipsum incidunt, ullam atque magnam itaque hic assumenda optio nostrum eligendi doloribus quo fuga dolore reprehenderit officia? Libero hic cumque ad vero autem quae? Culpa deleniti amet explicabo porro cupiditate molestiae sint laudantium. Quam molestiae ratione sed facilis. Eius minima possimus maiores pariatur delectus, dolor in modi quod dolorum repellendus? Magni, illo alias debitis officia perspiciatis doloribus fuga dolorum. Commodi quo sequi odit distinctio ex optio possimus repellat, laborum rem?'
-        ],
-        [
-            'title' => 'Judul Post Kedua',
-            'slug' => 'judul-post-kedua',
-            'author' => 'Uhuy',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias architecto nobis voluptate nesciunt eos repellat doloribus quas animi ducimus rem. Quidem iure nihil ducimus ea at eveniet voluptate libero laborum quibusdam repudiandae. Adipisci fugit rem provident, eligendi iure ducimus eveniet asperiores assumenda. Possimus ex asperiores quod animi odit enim optio corrupti, soluta voluptates quam tempore ipsa iusto provident reiciendis ullam repellat quo magnam adipisci, cupiditate dolorem incidunt voluptatibus maiores nemo ipsum! Veniam adipisci accusantium porro et. Quis quaerat omnis eos error maxime ratione tempore, asperiores labore sapiente temporibus! Alias, sequi odit ea totam iusto cupiditate cumque facere iure tempora. Magni?'
-        ],
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post['slug'] === $slug) {
-            $new_post = $post;
-        }
-    }
     return view('post', [
         'title' => $slug,
-        'post' => $new_post
+        'post' => Post::find($slug)
     ]);
 });
