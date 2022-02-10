9a03701c612438e54c031f6e2dba561e2621aa4f
imaldi <aldiirsanmajid@gmail.com>
2022-02-10 17:13:50 +0700

    done #5. Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -31,16 +32,6 @@ Route::get('/about', function () {
 
 
 
-Route::get('/posts', function () {
-    return view('posts', [
-        'title' => 'Posts',
-        'posts' => Post::all()
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('posts/{slug}', function ($slug) {
-    return view('post', [
-        'title' => $slug,
-        'post' => Post::find($slug)
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
