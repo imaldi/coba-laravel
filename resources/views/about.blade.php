2dae4b9d4f5b2f49de1e395a5b870d7f977dd0ae
imaldi <aldiirsanmajid@gmail.com>
2022-02-10 13:45:05 +0700

    adding bootstrap and using blade syntax

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,8 +9,8 @@
 </head>
 <body>
     <h1>Halaman About</h1>
-    <h3><?php echo $name; ?></h3>
-    <p><?= $email; ?></p>
-    <img src="img/<?= $img; ?>" alt="<?= $name; ?>" width="200">
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $img }}" alt="{{ $name }}" width="200">
 </body>
 </html>
