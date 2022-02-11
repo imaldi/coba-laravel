b014c152c473ff93ca0fdd4f2488606b3d78fa76
imaldi <aldiirsanmajid@gmail.com>
2022-02-11 13:41:11 +0700

    done #7. Post Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,11 +15,11 @@ class PostController extends Controller
         ]);
     }
 
-    public function show($slug)
+    public function show(Post $post)
     {
         return view('post', [
-            'title' => $slug,
-            'post' => Post::find($slug)
+            'title' => 'Single Post',
+            'post' => $post
         ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
         <article class="mb-5">
 
             <h2>
-                <a href="/posts/{{ $post->id }}">
+                <a href="/posts/{{ $post->slug }}">
                     {{ $post->title }}</a>
             </h2>
             {{-- <h5>By: {{ $post['author'] }}</h5> --}}
