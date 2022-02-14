6f2139bad7b85b210bf876c672476a0059309346
imaldi <aldiirsanmajid@gmail.com>
2022-02-14 09:25:40 +0700

    done eloquent for category and post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,6 +6,8 @@
             {{ $post->title }}
         </h2>
         {{-- <h5>By: {{ $post['author'] }}</h5> --}}
+        <h5>By: Aldi Irsan Majid in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </h5>
         {!! $post->body !!}
     </article>
     <a href="/posts">Back to Posts</a>
